1afee8366a1a90650c69003af527950fd65b9f2c
Nikola Kolev <nikolakolev712@gmail.com>
2022-06-13 17:42:36 +0300

    .

--- dice_game/assets/js/footer.js
+++ dice_game/assets/js/footer.js
@@ -1,3 +1,3 @@
 const currentYear = new Date().getFullYear()
 
-document.write(`<p> Copyright ${currentYear} Kolzo_Dev. All Rights  Reserved </p>`)
+document.write(`<a> Copyright ${currentYear} Kolzo_Dev. All Rights  Reserved </a>`)
